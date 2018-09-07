0b50f73efe12745675cea6477626928a0ad9992b
Steven Fisher <a_brit_abroad@hotmail.com>
2018-09-07 18:21:18 -0500

    powershell scripts

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+﻿
+$profileScript = (Split-Path -Path $profile) + "\a_brit_abroad_profile.ps1"
+
+& $profileScript
+
+Set-Alias -Name BH -Value BranchHistory
+Set-Alias -Name BD -Value BranchDiff
+
+function ngm { Invoke-Expression 'npm run mock' }
