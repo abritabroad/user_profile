bb4c8b8c21ace1512b8d6a5986940a23ab5d3e2c
Steven Fisher <steven.fisher@accruent.com>
2018-11-01 14:06:13 -0500

    added ps1 to ignore files in git

---
+++ Documents/WindowsPowerShell/git_ignore_files.ps1
@@ -0,0 +1,4 @@
+
+git update-index --skip-worktree "C:\git\360facility\ReactUI\package-lock.json"
+git update-index --skip-worktree "C:\git\360facility\ReactUI\package.json"
+git update-index --skip-worktree "C:\git\360facility\ReactUI\App\Utilities\devEnvironment.js"
