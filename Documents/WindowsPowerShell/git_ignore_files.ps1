21504155e32af6f933d6992f85936d8bf0b525bf
Steven Fisher <steven.fisher@accruent.com>
2018-11-09 15:55:42 -0600

    added undo for --skip-worktree

--- Documents/WindowsPowerShell/git_ignore_files.ps1
+++ Documents/WindowsPowerShell/git_ignore_files.ps1
@@ -2,3 +2,7 @@
 git update-index --skip-worktree "C:\git\360facility\ReactUI\package-lock.json"
 git update-index --skip-worktree "C:\git\360facility\ReactUI\package.json"
 git update-index --skip-worktree "C:\git\360facility\ReactUI\App\Utilities\devEnvironment.js"
+
+## undo --skip-worktree:
+#git update-index --no-skip-worktree "C:\git\360facility\ReactUI\package-lock.json"
+
