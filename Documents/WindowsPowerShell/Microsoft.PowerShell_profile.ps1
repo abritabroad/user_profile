e0670b4bfdaa31050260d3cff6dae38686335966
abritabroad <a_brit_abroad@hotmail.com>
2019-04-24 14:38:24 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -13,7 +13,13 @@ function ngm { Invoke-Expression 'npm run mock' }
 
 function gcd { Invoke-Expression 'git checkout develop' }
 
+function gpl { Invoke-Expression 'git pull' }
+
 function grh { Invoke-Expression 'git reset --hard' }
+
+function grd { Invoke-Expression 'git rebase develop' }
+
+function grdo { Invoke-Expression 'git rebase develop -Xours' }
 
 function gbh { Invoke-Expression "git for-each-ref --sort='-committerdate:iso8601' --format=' %(committerdate:iso8601)%09%(refname)' refs/heads" }
 
