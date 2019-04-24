a23ec62f43b9064a7767d0b191c1974cb614c1a7
abritabroad <a_brit_abroad@hotmail.com>
2019-04-24 14:34:21 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -11,11 +11,12 @@ remove-item env:ELECTRON_RUN_AS_NODE -erroraction 'silentlycontinue'
 
 function ngm { Invoke-Expression 'npm run mock' }
 
+function gcd { Invoke-Expression 'git checkout develop' }
+
 function grh { Invoke-Expression 'git reset --hard' }
 
-function bh { Invoke-Expression "git for-each-ref --sort='-committerdate:iso8601' --format=' %(committerdate:iso8601)%09%(refname)' refs/heads" }
+function gbh { Invoke-Expression "git for-each-ref --sort='-committerdate:iso8601' --format=' %(committerdate:iso8601)%09%(refname)' refs/heads" }
 
 # function bd { Invoke-Expression 'git diff --name-status master..' }
-function bd { Invoke-Expression 'git diff --name-status develop..' }
-
+function gbd { Invoke-Expression 'git diff --name-status develop..' }
 
