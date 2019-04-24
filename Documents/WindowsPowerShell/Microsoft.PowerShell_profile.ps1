bed883c4306662c96d81f57390ad2a59a0c17a78
abritabroad <a_brit_abroad@hotmail.com>
2019-04-24 14:32:01 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -7,9 +7,15 @@ $profileScript = $profileRoot + "\a_brit_abroad_profile.ps1"
 
 $env:path += ";$profileRoot"
 
-Set-Alias -Name BH -Value BranchHistory
-Set-Alias -Name BD -Value BranchDiff
-
 remove-item env:ELECTRON_RUN_AS_NODE -erroraction 'silentlycontinue'
 
 function ngm { Invoke-Expression 'npm run mock' }
+
+function grh { Invoke-Expression 'git reset --hard' }
+
+function bh { Invoke-Expression "git for-each-ref --sort='-committerdate:iso8601' --format=' %(committerdate:iso8601)%09%(refname)' refs/heads" }
+
+# function bd { Invoke-Expression 'git diff --name-status master..' }
+function bd { Invoke-Expression 'git diff --name-status develop..' }
+
+
