8f24f0569ac9fca5f87c31bf4850258e40f47416
abritabroad <a_brit_abroad@hotmail.com>
2020-12-29 14:38:07 -0600

    Create dev_init.sh

---
+++ dev_init.sh
@@ -0,0 +1,66 @@
+
+
+# dotNet 5 SDK
+
+sudo apt-get update;
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-5.0
+
+# if the above fails
+
+sudo apt-get install -y gpg
+wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget https://packages.microsoft.com/config/ubuntu/20.10/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+sudo apt-get update; \
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get update && \
+sudo apt-get install -y dotnet-sdk-5.0
+
+# node js
+
+sudo apt update
+sudo apt install nodejs npm
+
+# Azure CLI
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# kubectl
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Helm
+
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+# ~/.profile
+
+source ~/posh-git.sh        # https://github.com/lyze/posh-git-sh
+source ~/sf_functions.sh
+
+export token_npm=""
+export token_git=""
+export KENDO_UI_LICENSE=""
+
+export PROMPT_COMMAND='__posh_git_ps1 "\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]" "\\\$ ";'$PROMPT_COMMAND
+
+cd ~/source/repos/fnm/FishNet/Merchandise-Microservices
+
+
+# git config
+
+git config pull.rebase true
+
