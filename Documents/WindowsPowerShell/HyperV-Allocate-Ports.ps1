ad45c2bda565dbc6b80126ad397e336625020dfa
abritabroad <a_brit_abroad@hotmail.com>
2021-01-15 12:25:22 -0600

    Create HyperV-Allocate-Ports.ps1

---
+++ Documents/WindowsPowerShell/HyperV-Allocate-Ports.ps1
@@ -0,0 +1,22 @@
+
+# Display Currently Allocated
+# ---------------------------
+
+netsh interface ipv4 show excludedportrange protocol=tcp
+
+# Disable Hyper-V
+# ---------------
+
+dism.exe /Online /Disable-Feature:Microsoft-Hyper-V
+
+# Allocate Required ports
+# -----------------------
+
+netsh int ipv4 add excludedportrange protocol=tcp startport=3000 numberofports=2
+netsh int ipv4 add excludedportrange protocol=tcp startport=8080 numberofports=1
+
+# Enable Hyper-V
+# ---------------
+
+dism.exe /Online /Enable-Feature:Microsoft-Hyper-V
+
