4ee885e3d0ccee5ae4cc18f2c573ac6ac6552823
abritabroad <a_brit_abroad@hotmail.com>
2021-01-19 13:09:59 -0600

    Update HyperV-Allocate-Ports.ps1

--- Documents/WindowsPowerShell/HyperV-Allocate-Ports.ps1
+++ Documents/WindowsPowerShell/HyperV-Allocate-Ports.ps1
@@ -12,9 +12,10 @@ dism.exe /Online /Disable-Feature:Microsoft-Hyper-V
 # Allocate Required ports
 # -----------------------
 
-netsh int ipv4 add excludedportrange protocol=tcp startport=3000 numberofports=2
-netsh int ipv4 add excludedportrange protocol=tcp startport=6942 numberofports=50
-netsh int ipv4 add excludedportrange protocol=tcp startport=8080 numberofports=1
+netsh int ipv4 add excludedportrange protocol=tcp startport=3000 numberofports=2    # SPA ports
+netsh int ipv4 add excludedportrange protocol=tcp startport=6942 numberofports=50   # IntelliJ server process
+netsh int ipv4 add excludedportrange protocol=tcp startport=8080 numberofports=1    # API ports
+netsh int ipv4 add excludedportrange protocol=tcp startport=9009 numberofports=1    # IntelliJ debugger
 
 # Enable Hyper-V
 # ---------------
