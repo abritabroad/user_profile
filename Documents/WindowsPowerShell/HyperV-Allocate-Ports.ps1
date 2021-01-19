6536a9793a5aa02dd4c413720e11ad0b31f77d26
abritabroad <a_brit_abroad@hotmail.com>
2021-01-19 14:47:07 -0600

    Update HyperV-Allocate-Ports.ps1

--- Documents/WindowsPowerShell/HyperV-Allocate-Ports.ps1
+++ Documents/WindowsPowerShell/HyperV-Allocate-Ports.ps1
@@ -13,8 +13,10 @@ dism.exe /Online /Disable-Feature:Microsoft-Hyper-V
 # -----------------------
 
 netsh int ipv4 add excludedportrange protocol=tcp startport=3000 numberofports=2    # SPA ports
+netsh int ipv4 add excludedportrange protocol=tcp startport=4848 numberofports=1    # Glassfish Admin port
 netsh int ipv4 add excludedportrange protocol=tcp startport=6942 numberofports=50   # IntelliJ server process
-netsh int ipv4 add excludedportrange protocol=tcp startport=8080 numberofports=1    # API ports
+netsh int ipv4 add excludedportrange protocol=tcp startport=8080 numberofports=1    # Glassfish Server HTTP port
+netsh int ipv4 add excludedportrange protocol=tcp startport=8181 numberofports=1    # Glassfish Server HTTPS port
 netsh int ipv4 add excludedportrange protocol=tcp startport=9009 numberofports=1    # IntelliJ debugger
 
 # Enable Hyper-V
