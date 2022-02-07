f219968f5843d8bc36eeb90c1c5d5097520fd70c
abritabroad <a_brit_abroad@hotmail.com>
2022-02-07 17:23:38 -0600

    Update dev_init.sh
    cd into repo on terminal startup

--- dev_init.sh
+++ dev_init.sh
@@ -57,8 +57,9 @@ export KENDO_UI_LICENSE=""
 
 export PROMPT_COMMAND='__posh_git_ps1 "\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]" "\\\$ ";'$PROMPT_COMMAND
 
-cd ~/source/repos/fnm/FishNet/Merchandise-Microservices
-
+if [ $(pwd) == "/mnt/c/Users/a_bri" ] || [ $(pwd) == "/home/steve" ] ; then
+    cd ~/source/repos/fnm/FishNet/Merchandise-Microservices
+fi
 
 # git config
 
